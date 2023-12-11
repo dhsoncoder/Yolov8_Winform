d146303f09f4633ebd21bf980b040503a863cee7
Dang Hai Son <92775445+dhsoncoder@users.noreply.github.com>
2023-12-11 14:03:29 +0700

    Add files via upload

---
+++ Library class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yolov8_Form
+{
+    internal class Class1
+    {
+    }
+}
